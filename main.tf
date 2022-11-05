8805c848e1d0e5d348ebb9efb2410b8e6bdb29db
artonjashari <ajashari94@gmail.com>
2022-11-05 12:35:44 +0100

    Add IAM USER ASSIGNMENT MODIFED MAIN

--- main.tf
+++ main.tf
@@ -19,3 +19,33 @@ resource "aws_iam_user_policy" "lb_ro" {
   # Terraform expression result to valid JSON syntax.
   policy = jsonencode(var.policy_document)
 }
+
+module "user-arton-modulee" {
+  source  = "app.terraform.io/artonjashari/user-arton-modulee/iam"
+  version = "1.0.2"
+  # insert required variables here
+
+  user_name = "arton-user"
+  policy_name = "arton-policy"
+
+  policy_document = {
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*", "iam:GetAccountPasswordPolicy", "elasticloadbalancing:DescribeLoadBalancers"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "iam:ChangePassword"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  
+  }
+}
